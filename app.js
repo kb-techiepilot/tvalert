773edc9a7d2062219340f087333ec0b69b739cdf
balaji <balaji@localhost>
2022-01-26 15:32:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", function(req, res){
 });
 
 app.post('/', async (req, res) => {
-    console.log("kb-request :" + req);
+    console.log(req);
     const gainers = await axios.post('https://api.telegram.org/bot5007955067:AAGCvnc1AhD0Y11ukuM4zuuCKTb3obJVlwM/sendMessage?chat_id=-1001793121016&text='+req.body.text);
     console.log(gainers);
     res.json({"return" : "success"});
