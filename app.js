10e631f8dffb0e09225d047a2323463c6c0c0ee8
balaji <balaji@localhost>
2022-01-26 14:57:34 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.send("welcome to backend server");
+});
+
+app.post('/', async (req, res) => {
+    console.log(req.body);
+    const gainers = await axios.post('https://api.telegram.org/bot5007955067:AAGCvnc1AhD0Y11ukuM4zuuCKTb3obJVlwM/sendMessage?chat_id=-1001793121016&text='+req.body.text);
+    console.log(gainers);
+    res.json({"return" : "success"});
+});
+
+app.listen(3001, function(){
+    console.log("server started on port 3001");
+});
