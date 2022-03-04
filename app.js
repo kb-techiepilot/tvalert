20b02bdb4c2de57532543ab79e5a78de88f3b8dc
balaji <balaji@localhost>
2022-03-04 06:17:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,15 +30,15 @@ app.post('/', jsonParser, async (req, res) => {
 
 app.post('/us30', bodyParser.text({type: '*/*'}), async (req, res) => {
     console.log(req.body);
-    if(req.body.msg !== undefined ) {
+    // if(req.body.msg !== undefined ) {
         await axios.post('https://api.telegram.org/bot5007955067:AAGCvnc1AhD0Y11ukuM4zuuCKTb3obJVlwM/sendMessage?chat_id='+ us30signals +'&text='+req.body);
         res.json({"return" : "success"});
-    } else {
-        res.json({"return" : "failiure"});
-    }
+    // } else {
+    //     res.json({"return" : "failiure"});
+    // }
 });
 
-app.post('/test', bodyParser.text({type: '*/*'}), async (req, res) => {
+app.post('/test', jsonParser, async (req, res) => {
     console.log(req.body);
 });
 
