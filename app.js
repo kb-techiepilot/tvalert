767c23ed28f153ea27fbd96e4b3789c811a8f48e
balaji <balaji@localhost>
2022-03-10 09:23:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const app = express();
 
 const dstreetalertcheck = "-1001793121016";
-const dstreetlivechat = "-1001643333515";
+// const dstreetlivechat = "-1001643333515";
 const us30signals = "-1001754059003";
 
 var jsonParser = bodyParser.json()
@@ -17,11 +17,11 @@ app.get("/", function(req, res){
     res.send("welcome to backend server");
 });
 
-app.post('/', jsonParser, async (req, res) => {
+app.post('/', bodyParser.text({type: '*/*'}), async (req, res) => {
     console.log(req.body);
-    if(req.body.msg !== undefined ) {
-        await axios.post('https://api.telegram.org/bot5007955067:AAGCvnc1AhD0Y11ukuM4zuuCKTb3obJVlwM/sendMessage?chat_id='+ dstreetlivechat +'&text='+req.body.msg);
-        await axios.post('https://api.telegram.org/bot5007955067:AAGCvnc1AhD0Y11ukuM4zuuCKTb3obJVlwM/sendMessage?chat_id='+ dstreetalertcheck +'&text='+req.body.msg);
+    if(req.body !== undefined ) {
+        // await axios.post('https://api.telegram.org/bot5007955067:AAGCvnc1AhD0Y11ukuM4zuuCKTb3obJVlwM/sendMessage?chat_id='+ dstreetlivechat +'&text='+req.body.msg);
+        await axios.post('https://api.telegram.org/bot5007955067:AAGCvnc1AhD0Y11ukuM4zuuCKTb3obJVlwM/sendMessage?chat_id='+ dstreetalertcheck +'&text='+req.body);
         res.json({"return" : "success"});
     } else {
         res.json({"return" : "failiure"});
